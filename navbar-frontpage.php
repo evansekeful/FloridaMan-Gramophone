542268a7ad7c8bd8709c30d1d01656a029368352
evansekeful <ellie@solnimbus.com>
2017-04-30 19:12:41 -0400

    css adjustments for header and menu

--- navbar-frontpage.php
+++ navbar-frontpage.php
@@ -33,7 +33,7 @@
   <!-- Header Logo -->
   <div class="jumbotron-fluid frontpage header">
       <?php if ( get_header_image() ) : ?>
-          <div>
+          <div class="container">
               <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
                   <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
               </a>
