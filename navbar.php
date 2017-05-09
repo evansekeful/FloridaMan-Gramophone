6e1437d3d837c4019a0f1e9beff32259a98e0ecc
evansekeful <ellie@solnimbus.com>
2017-05-09 10:28:33 -0400

    fixed responsive nav

--- navbar.php
+++ navbar.php
@@ -1,35 +1,28 @@
-<nav class="navbar navbar-light navbar-toggleable-md bd-navbar clearfix transparent">
-      <div class="container-fluid">
-        <div class="navbar-header container-fluid">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bd-main-nav" aria-controls="bd-main-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-            <?php if ( ! has_custom_logo() ) { ?>
-                <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-            <?php }
-                  else {
-                    the_custom_logo();
-                  } ?>
-        </div>
-      </div>
+  <?php if ( ! has_custom_logo() ) { ?>
+      <a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+  <?php }
+        else {
+          the_custom_logo();
+        } ?>
 
-        <div class="collapse navbar-collapse nav navbar-nav" id="bd-main-nav">
-            <ul class="nav navbar-nav mr-auto">
-
-                <?php
-                    wp_nav_menu( array(
-                        'menu'           => 'primary',
-                        'theme_location' => 'primary',
-                        'depth'          => 1,
-                        'container'      => "ul",
-                        'menu_class'     => "navbar-nav mr-auto",
-                        'fallback_cb'    => 'bootstrap_walker::fallback',
-                        'walker'         => new Bootstrap_Walker()
-                    ) );
-                ?>
-
-            </ul>
-        </nav>
-      </div>
+  <div class="collapse navbar-collapse" id="navbarNav"> 
+      <ul class="navbar-nav">
+            <?php
+                wp_nav_menu( array(
+                    'menu'           => 'primary',
+                    'theme_location' => 'primary',
+                    'depth'          => 1,
+                    'container'      => "ul",
+                    'menu_class'     => "navbar-nav mr-auto",
+                    'fallback_cb'    => 'bootstrap_walker::fallback',
+                    'walker'         => new Bootstrap_Walker()
+                ) );
+            ?>
+        </ul>
+  </div>
 </nav>
